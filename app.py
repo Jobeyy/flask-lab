a12c44f4deb5c4a0c92820f17468fab1ee154105
Jobey Farias <JobeyFarias@gmail.com>
2023-04-11 11:47:05 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def mainPage():
+    name = "Jobey Farias"
+    return render_template('base.html', name=name, aboutMe=readDetails('C:\\Users\\jobey\\OneDrive\\Documents\\flask lab\\static\\content.txt'))
+    
+def readDetails(filepath):
+        with open(filepath, 'r') as f:
+            return [line for line in f]
+        
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo():
+     name= None
+     if request.method == 'POST':
+          name = request.form['name']
+          return render_template('base.html', name=name, aboutMe=readDetails('C:\\Users\\jobey\\OneDrive\\Documents\\flask lab\\static\moreContent.txt'))
+          
+     
+     return render_template('form.html', name=name)
+
+@app.route('/user/<name>')
+def greet(name):
+     return f"Hello {name}"
+if __name__ == "__main__":
+    app.run(debug=True)
